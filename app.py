3765917174034875e1b1e372d2d57f01f9a384eb
jmrosadoflorez-afk <jmrosadoflorez@gmail.com>
2025-08-18 19:12:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -303,4 +303,7 @@ def servicios_eliminar(id):
     return redirect(url_for("servicios_list"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
